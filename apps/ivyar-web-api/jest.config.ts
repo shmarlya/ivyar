4d24e615f99da05a7166a57a379347c6c46a08be
shmarlya <yarmovan@gmail.com>
2023-02-23 23:10:35 +0100

    boilerplate setup client and api applications, dependencies

---
+++ apps/ivyar-web-api/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'ivyar-web-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/ivyar-web-api',
+};
---
+++ apps/ivyar-web-client/src/app/app.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+export function App() {
+  return <>Hello its web client</>;
+}
+
+export default App;
